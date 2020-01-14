aaeae7f0bb28fa934f94fa5d1b5132f152e02977
Mihai Leonte <leontemihai@gmail.com>
2020-01-14 14:18:09 +0200

    Added popOverPresentationController to the Settings alertControllers - to avoid crashing on the iPad

---
+++ WelcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeView.swift
+//  Kuchi
+//
+//  Created by Mihai Leonte on 13/01/2020.
+//  Copyright © 2020 Omnijar. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
