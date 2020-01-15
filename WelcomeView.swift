eded3d640ead59e60830b62302c8a8cda2b791f2
Mihai Leonte <leontemihai@gmail.com>
2020-01-15 11:32:08 +0200

    chapter completed

--- WelcomeView.swift
+++ WelcomeView.swift
@@ -10,7 +10,47 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("welcome-background", bundle: nil)
+                .resizable()
+                //.scaledToFit()
+                .aspectRatio(1 / 1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .saturation(0.5)
+                .blur(radius: 5)
+                .opacity(0.08)
+            
+            HStack {
+                Image(systemName: "table")
+                    .resizable()
+                    .frame(width: 50.0, height: 50.0)
+                    // 1
+                    .cornerRadius(60 / 2)
+                    // 2
+                    .border(Color.gray, width: 1)
+                    // 3
+                    .background(Color(white: 0.9))
+                    // 4
+                    .clipShape(Circle())
+                    // 5
+                    .foregroundColor(.red)
+                
+                VStack {
+                    Text("Welcome to")
+                        .font(.headline)
+                        .bold()
+                    Text("Kuchi")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)
+            }
+        }
+        
+        
     }
 }
 
